51651fd4e48b88c9b6bdb28ba6e96f43bf233464
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-07-20 20:46:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max and min element in Binary Tree/max-and-min-element-in-binary-tree.java
+++ Difficulty: Easy/Max and min element in Binary Tree/max-and-min-element-in-binary-tree.java
@@ -9,75 +9,15 @@ class Node {
 }
 */
 class Solution {
-  static void helper(Node root,int max[]){
-        if(root==null) return;
-        if(root.data>max[0]) max[0]=root.data;
-        helper(root.left,max);
-        helper(root.right,max);
-    }
-  static void helper1(Node root,int mn[]){
-        if(root==null) return;
-        if(root.data<mn[0]) mn[0]=root.data;
-        helper1(root.left,mn);
-        helper1(root.right,mn);
-    }
     public static int findMax(Node root) {
-      int max[]=new int[1];
-        max[0]=Integer.MIN_VALUE;
-      helper(root,max);
-    //   System.out.print(max[0]);
-      return max[0];
-        
+        // code here
+        if(root==null) return Integer.MIN_VALUE;
+        return Math.max(root.data,Math.max(findMax(root.left),findMax(root.right)));
     }
 
     public static int findMin(Node root) {
-        int mn[]=new int[1];
-        mn[0]=Integer.MAX_VALUE;
-      helper1(root,mn);
-        // System.out.print(min[0]);
-      return mn[0];
-       
+          if(root==null) return Integer.MAX_VALUE;
+        return Math.min(root.data,Math.min(findMin(root.left),findMin(root.right)));
+        // code here
     }
 }
-/*
-class Node {
-    int data;
-    Node left, right;
-
-    public Node(int data){
-        this.data = data;
-    }
-}
-*/
-// class Solution {
-//     static void helper1(Node root,int arr[]){
-//         if(root==null) return;
-//         // arr[0]=Math.max(root.data,arr[0]);
-//         if(root.data>arr[0]) arr[0]=root.data;
-//         helper1(root.left,arr);
-//         helper1(root.right,arr);
-//     }
-//     static void helper2(Node root,int arr[]){
-//         if(root==null) return;
-//         // arr[0]=Math.min(root.data,arr[0]);
-//         if(root.data<arr[0]) arr[0]=root.data;
-//         helper2(root.left,arr);
-//         helper2(root.right,arr);
-//     }
-//     public static int findMax(Node root) {
-//         // code here
-//         int arr[]=new int[1];
-//         helper1(root,arr);
-//         return arr[0];
-       
-//     }
-
-//     public static int findMin(Node root) {
-       
-//         // code here
-//         int arr[]=new int[1];
-//         arr[0]=Integer.MAX_VALUE;
-//         helper2(root,arr);
-//         return arr[0];
-//     }
-// }

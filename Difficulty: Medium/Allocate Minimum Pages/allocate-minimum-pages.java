39e6b7681eae59ca88231a73e1dc5e536ebdb665
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-01-22 00:58:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.java
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.java
@@ -1,31 +1,29 @@
 class Solution {
     public int findPages(int[] arr, int k) {
         // code here
-        if(arr.length<k) return -1;
+        if(k>arr.length) return -1;
+        int min=Integer.MIN_VALUE;
         int total=0;
-        int max=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
+            min=Math.max(min,arr[i]);
             total+=arr[i];
-            if(max<arr[i]) max=arr[i];
         }
-        int low=max;
+        int low=min;
         int high=total;
-        while(low<=high){
-            int mid=(low+high)/2;
+        while(low<high){
             int s=1;
-            int sum=0;
-            for(int i=0;i<arr.length;i++){
+            int sum=arr[0];
+            int mid=(low+high)/2;
+            for(int i=1;i<arr.length;i++){
                 sum+=arr[i];
-                if(sum>mid) {
+                if(sum>mid){
                     s++;
                     sum=arr[i];
                 }
             }
-            if(s<=k) high=mid-1;
-            else low=mid+1;
+            if(s>k) low=mid+1;
+            else high=mid;
         }
         return low;
     }
-
-    
 }

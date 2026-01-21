cb1ac90b8d38b1390b433115cacaeef27496e9e4
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-01-21 15:14:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.java
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -1,33 +1,21 @@
 class Solution {
-    public ArrayList<Integer> nextLargerElement(int[] nums) {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
         // code here
+        int n=arr.length;
+        ArrayList<Integer>nge=new ArrayList<>();
         Stack<Integer>st=new Stack<>();
-        ArrayList<Integer>ans=new ArrayList<>();
-        for(int i=nums.length-1;i>=0;i--){
-            if(st.isEmpty()){
-                ans.add(-1);
-                st.push(nums[i]);
-            }
-            else if(nums[i]<st.peek()){
-                ans.add(st.peek());
-                st.push(nums[i]);
-            }
-            else{
-                while(st.size()>0 && nums[i]>=st.peek()){
-                    st.pop();
-                }
-                if(st.isEmpty()){ 
-                    ans.add(-1);
-                    st.push(nums[i]);
-                }
-                else{
-                ans.add(st.peek());
-                st.push(nums[i]);
-                }
-            }
-            }
-        
-         Collections.reverse(ans);
-         return ans;
-}
+        for(int i=0;i<n;i++) nge.add(0);
+        st.push(arr[n-1]);
+        nge.set(n-1,-1);
+        for(int i=n-2;i>=0;i--){
+            while(st.size()>0 && st.peek()<=arr[i]) st.pop();
+            if(st.size()==0) nge.set(i,-1);
+           else{
+                nge.set(i,st.peek());
+           
+           }
+            st.push(arr[i]);
+        }
+        return nge;
+    }
 }

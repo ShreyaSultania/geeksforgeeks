8bac6cedbe7af208e0126d9808573943b42e5f58
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-05-18 17:13:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.java
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.java
@@ -1,5 +1,5 @@
 class Solution {
-    int firstOccurence(int []arr,int target){
+    int lowerBound(int []arr,int target){
         int low=0;
         int high=arr.length-1;
         int ans=-1;
@@ -9,31 +9,35 @@ class Solution {
                 ans=mid;
                 high=mid-1;
             }
-            else if(arr[mid]>target) high=mid-1;
+            else if(arr[mid]>target){
+                high=mid-1;
+            }
             else low=mid+1;
         }
         return ans;
     }
-     int lastOccurence(int []arr,int target){
+    int upperBound(int []arr,int target){
         int low=0;
         int high=arr.length-1;
         int ans=-1;
         while(low<=high){
-            int mid=(low+high)/2;
+             int mid=(low+high)/2;
             if(arr[mid]==target){
                 ans=mid;
                 low=mid+1;
             }
-            else if(arr[mid]>target) high=mid-1;
+            else if(arr[mid]>target){
+                high=mid-1;
+            }
             else low=mid+1;
         }
         return ans;
     }
     int countFreq(int[] arr, int target) {
         // code here
-        int first=firstOccurence(arr,target);
-        int last=lastOccurence(arr,target);
-        if(first==-1 || last==-1) return 0;
-        return last-first+1;
+        int low=lowerBound(arr,target);
+        int high=upperBound(arr,target);
+        if(low!=-1 && high!=-1) return high-low+1;
+        else return 0;
     }
 }

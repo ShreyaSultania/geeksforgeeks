cc9440dd0a237609d894b494ae7cfbb020f56199
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-06-30 11:08:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.java
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.java
@@ -1,4 +1,4 @@
-/*
+/* Structure of linked list Node
 class Node{
     int data;
     Node next;
@@ -9,10 +9,9 @@ class Node{
     }
 }
 */
-
 class Solution {
-    public Node Reverse(Node head){
-         Node prev=null;
+    Node reverse(Node head){
+        Node prev=null;
         Node curr=head;
         Node Next=null;
         while(curr!=null){
@@ -23,24 +22,23 @@ class Solution {
         }
         return prev;
     }
-    public Node addOne(Node head) {
+        public Node addOne(Node head) {
+        Node dummy=new Node(-1);
         // code here.
-        Node temp=Reverse(head);
-        Node dummy=new Node(0);
         Node t=dummy;
+        Node temp=reverse(head);
         int carry=1;
         while(temp!=null){
-            Node a=new Node((temp.data+carry)%10);
-            t.next=a;
-            t=t.next;
-            carry=(temp.data+carry)/10;
+            int sum=temp.data+carry;
             temp=temp.next;
+            t.next=new Node(sum%10);
+            t=t.next;
+            carry=sum/10;
         }
-        if(carry>0) {
-        Node a=new Node((carry)%10);
-            t.next=a;
+        if(carry>0){
+            t.next=new Node(carry%10);
             t=t.next;
         }
-        return Reverse(dummy.next);
+        return reverse(dummy.next);
     }
 }

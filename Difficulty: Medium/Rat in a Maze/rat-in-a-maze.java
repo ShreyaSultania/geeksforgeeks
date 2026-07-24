ab97340514abdc8b3fc5d7b34a4623bf35dff886
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-07-24 10:55:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze/rat-in-a-maze.java
+++ Difficulty: Medium/Rat in a Maze/rat-in-a-maze.java
@@ -1,29 +1,26 @@
 class Solution {
-    void helper(int r,int c,ArrayList<String>ans,int[][] maze,String temp,boolean[][] visited){
-        int row=maze.length;
-        int col=maze[0].length;
-        if(r>=row || c>=col ||c<0 || r<0 || maze[r][c]==0 || visited[r][c]) return;
-        if(r==row-1 && c==col-1){
-            // Collections.reverse()
-            ans.add(temp);
+    ArrayList<String>ans;
+    void dfs(int i,int j,boolean[][]visited,int[][]maze,int row,int col,String str){
+        if(i<0 || j<0 ||i>=row || j>=col || maze[i][j]==0 || visited[i][j]) return;
+        if(i==row-1 && j==col-1){
+            ans.add(str);
             return;
         }
-        visited[r][c]=true;
-        helper(r+1,c,ans,maze,temp+"D",visited);
-        helper(r-1,c,ans,maze,temp+"U",visited);
-        helper(r,c+1,ans,maze,temp+"R",visited);
-        helper(r,c-1,ans,maze,temp+"L",visited);
-        visited[r][c]=false;
+        visited[i][j]=true;
+            dfs(i+1,j,visited,maze,row,col,str+"D");
+            dfs(i,j+1,visited,maze,row,col,str+"R");
+            dfs(i-1,j,visited,maze,row,col,str+"U");
+            dfs(i,j-1,visited,maze,row,col,str+"L");
+            visited[i][j]=false;
     }
     public ArrayList<String> ratInMaze(int[][] maze) {
         // code here
-         int row=maze.length;
+        ans=new ArrayList<>();
+        int row=maze.length;
         int col=maze[0].length;
-        ArrayList<String>ans=new ArrayList<>();
-        boolean[][] visited=new boolean[row][col];
-        helper(0,0,ans,maze,"",visited);
+        boolean[][]visited=new boolean[row][col];
+        dfs(0,0,visited,maze,row,col,"");
         Collections.sort(ans);
-        
         return ans;
     }
 }

3aac4e43ddd31795634d405ee62e7e6d62c814b9
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-07-28 23:21:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Encode and Decode Strings/encode-and-decode-strings.java
+++ Difficulty: Medium/Encode and Decode Strings/encode-and-decode-strings.java
@@ -1,18 +1,22 @@
 class Solution {
 
-    public String encode(String s[]) {
+    public String encode(String arr[]) {
         // write your logic to encode the strings
-        String ans="";
-        for(int i=0;i<s.length;i++){
-            ans=ans+s[i]+" ";
+        int n=arr.length;
+        String s="";
+        for(int i=0;i<n;i++){
+            s=s+arr[i]+" ";
+        }
+        return s;
+    }
+
+    public ArrayList<String> decode(String s) {
+        // write your logic to decode the string
+        String arr[]=s.split(" ");
+        ArrayList<String>ans=new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            ans.add(arr[i]);
         }
         return ans;
-    }
-    
-
-    public String[] decode(String s) {
-        // write your logic to decode the string
-        String []str=s.split(" ");
-        return str;
     }
 }
